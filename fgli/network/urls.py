e2351711fcb9d117ac2b5bada3d498c36df0b9fe
Daniel Quiroga <dquiroga@email.wm.edu>
2021-04-09 20:08:10 -0400

    Add logic for login and logout capabilities

--- fgli/network/urls.py
+++ fgli/network/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.network, name='network'),
+    path('', views.network, name='home'),
+    path('login/', views.login_request, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_request, name='logout'),
 ]
